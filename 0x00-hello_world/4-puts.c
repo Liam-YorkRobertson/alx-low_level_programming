2252020277c0032ad8410f68249791c556e1930a
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-16 12:58:24 -0800

    C program that prints stuff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * This function prints text
+ */
+
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(Mystr);
 	return (0);
 }	

c74a1e0e37716d0f7d70b1d4efc0362cde849dae
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-16 14:53:11 -0800

    C program that prints text, without printf or putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main- entry point
- * Return
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 
 int main(void)
 {
-    fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-    return 1;
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }

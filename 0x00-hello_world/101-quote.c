1cd677b86276f872f42c67e2eff663e5ef46ca98
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-16 14:38:06 -0800

    C program that prints text, without printf or putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ * main- entry point
+ * Return
+ */
+
+int main(void)
+{
+    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    syscall(SYS_write, 1, msg, sizeof(msg));
+    return 0;
+}

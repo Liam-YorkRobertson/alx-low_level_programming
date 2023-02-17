e5e14c80d35031859fa92e9128b9ef7dd902190a
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-17 07:07:39 -0800

    program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main = entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char alphab = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(alphab);
+	return (0);
+}

1e69a31e9ffb4908e5af91af11e0585c1e60e071
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-17 13:29:17 -0800

    program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,12 +18,12 @@ int main(void)
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
 				putchar((num3 % 10) + '0');
-
+				
 				if (num1 == 7 && num2 == 8 && num3 ==9)
 				{
 					continue;
 				}
-				
+
 				putchar(',');
 				putchar(' ');
 			}
@@ -31,4 +31,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}

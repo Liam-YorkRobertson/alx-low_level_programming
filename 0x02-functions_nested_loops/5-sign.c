e5da7327c13932b8672831e877b84fe496eb9bb8
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-21 08:57:38 -0800

    function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_sign - function that prints the sign of a number
+ *@n: parameter to be printed
+ *Return: 1 if positive, -1 if negative, 0 if 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}

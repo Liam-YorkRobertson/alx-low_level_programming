13f7eb1e59a03b86aff293b909125c160b9913cf
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-21 09:04:05 -0800

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,14 +3,14 @@
 /**
  *print_sign - function that prints the sign of a number
  *@n: parameter to be printed
- *Return: 1 if positive, -1 if negative, 0 if 0
+ *Return: +1 if positive, -1 if negative, 0 if 0
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
+		return (+1);
 	}
 	else if (n < 0)
 	{

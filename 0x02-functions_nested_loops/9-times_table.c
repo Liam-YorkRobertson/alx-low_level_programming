a1839e552409705d374dbc900635e29f8e209983
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-21 16:17:44 -0800

    function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,29 @@
 
 void times_table(void)
 {
-	int h = 0;
-	int v;
+	int c, r, p;
 
-	while (h <= 9)
+	for (c = 0; c <= 9; c++)
 	{
-		v = 0;
+		_putchar('0');
 
-		while (v <= 9)
+		for (r = 1; r <= 9; r++)
 		{
-			char s = h * v;
-
-			_putchar(s + '0');
 			_putchar(',');
 			_putchar(' ');
-			v++;
+
+			p = c * r;
+
+			if (p <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((p / 10) + '0');
+			}
+			_putchar((p % 10) + '0');
 		}
 		_putchar('\n');
-		h++;
 	}
 }

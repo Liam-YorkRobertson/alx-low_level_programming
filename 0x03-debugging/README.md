c5e05fc487aaf8e6a4b857811a20b75f129244b6
“Liam-York <liamyorkdeonrobertson@gmail.com>
2023-02-22 02:43:58 -0800

    file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+In this repo we are going to be debugging problems created for us by ALX
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *positive_or_negative - prints if number is positive, negative, or zero
+ *@i: parameter to be printed
+ *Return: none (void)
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}

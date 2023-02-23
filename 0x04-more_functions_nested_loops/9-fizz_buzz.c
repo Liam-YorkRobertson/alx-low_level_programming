3e00493f2cce03597d14e9b6c6672abd498c822b
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-02-23 09:52:38 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,23 @@
 int main(void)
 {
 	int i;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char fizzbuzz[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("%s ", fizzbuzz);
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s  ", fizz);
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("%s ", buzz);
 		}
 		else
 		{

4e07a9d3262f9c98a830f2de969b9630327381b0
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-02-27 07:51:13 -0800

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value the pointer points to to 98
+ * @n: pointer parameter to be changed
+ * Return: non (void)
+ */
+
+void reset_to_98(int *n)
+{
+	int i;
+
+	i = 98;
+	*n = i;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory we're going to be creating functions to understand and learn pointers, arrays, and strings

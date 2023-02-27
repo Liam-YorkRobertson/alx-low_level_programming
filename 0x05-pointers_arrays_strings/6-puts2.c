6c2129dbf36ec55fb8b873a872cdce3cd1551186
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-02-27 13:47:12 -0800

    function that prints every other character of a string, starting with the first character, followed by a new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every second number
+ * @str: number to be split
+ * return: none (void)
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			break;
+		}
+	}
+	_putchar('\n');
+}

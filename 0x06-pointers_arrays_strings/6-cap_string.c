2e732d4b905c07e5dad22ef6c3e38f1f8c4b74e7
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-01 13:51:49 -0800

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,9 +6,35 @@
  *Return: n (char)
  */
 
-char *cap_string(char *)
+char *cap_string(char *n)
 {
-	int i;
+	int ind;
 
-	i = 0;
-	while (
+	ind = 0;
+	while (n[ind])
+	{
+		while (!(n[ind] >= 'a' && n[ind] <= 'z'))/*runs when NOT alph*/
+		{
+			ind++;
+		}
+
+		if (n[ind - 1] == ' ' ||
+				n[ind - 1] == '\t' ||
+				n[ind - 1] == '\n' ||
+				n[ind - 1] == ',' ||
+				n[ind - 1] == ';' ||
+				n[ind - 1] == '.' ||
+				n[ind - 1] == '!' ||
+				n[ind - 1] == '?' ||
+				n[ind - 1] == '"' ||
+				n[ind - 1] == '(' ||
+				n[ind - 1] == ')' ||
+				n[ind - 1] == '{' ||
+				n[ind - 1] == '}') /*-1, because before word*/
+		{
+			n[ind] = n[ind] - 32; /*capitalizes*/
+		}
+			ind++;
+	}
+	return (n);
+}

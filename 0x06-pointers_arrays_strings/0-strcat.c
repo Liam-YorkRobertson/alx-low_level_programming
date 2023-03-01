cfe951a628b52159e51634a888eff8fd02aa8a4f
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-01 11:02:16 -0800

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 {
 	int dlen = 0;
 	int slen = 0;
-	int i = 0;
+	int i;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
@@ -23,7 +23,7 @@ char *_strcat(char *dest, char *src)
 	}
 	for (i = 0; i <= slen; i++) /*i <= slen stops appending when slen don*/
 	{
-		dest[dlen + 1] = src[i]; /**dlen + i, because we want to add
+		dest[dlen + i] = src[i]; /**dlen + i, because we want to add
 		*src to the end of dest, if it wasn't + i, then would overwrite dest
 		*/
 	}

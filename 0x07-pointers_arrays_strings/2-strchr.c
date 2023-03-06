b0b4f8f10e8f4016d9b283f5596e13886253d7f2
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-06 08:50:47 -0800

    function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,18 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	if (s[i] == c)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		return (s);
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		else
+		{
+			return ('\0');
+		}
 	}
-	else
-	{
-		return ('\0');
-	}
+	return (0);
 }

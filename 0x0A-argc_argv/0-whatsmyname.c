dafecfb3ee0d7f9cf44935b66dc0d30257db6ceb
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-10 10:42:34 -0800

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int main (int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argc == i)
+	{
+		printf("%s", argv[argc]);
+	}
+	return (0);
+}

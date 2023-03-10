1a8d6c5287c206b32a119aec4f3dd2ecbb4cb4eb
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-10 11:49:30 -0800

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,34 +3,33 @@
 #include "main.h"
 
 /**
- *main - prints arguments
- *@argc: argument count
- *@argv: argument vector
- *Return: 0 (success)
- */
+*main - prints arguments
+*@argc: argument count
+*@argv: argument vector
+*Return: 0 (success)
+*/
 
 int main(int argc, char *argv[])
 {
-        int num1 = 0;
-        int num2 = 0;
-        int sum;
+	int num1 = 0;
+	int num2 = 0;
+	int sum;
 
-        if (argc == 3)
-        {
-                num1 = atoi(argv[1]); /*have to change from char to int*/
-                num2 = atoi(argv[2]);
-                sum = num1 + num2;
-                printf("%d\n", sum);
-        }
+	if (argc == 3 && (!(*argv[1] < 48 || *argv[1] > 57) || !(*argv[2] < 48 || *argv[2] > 57)))
+	{
+		num1 = atoi(argv[1]); /*have to change from char to int*/
+		num2 = atoi(argv[2]);
+		sum = num1 + num2;
+		printf("%d\n", sum);
+	}
 	else if (argc == 1)
 	{
 		printf("0\n");
 	}
-	else if ((*argv[1] < 48 || *argv[1] > 57) || 
-			(*argv[2] < 48 || *argv[2] > 57) )
-        {
-                printf("Error\n");
-                return (1);
-        }
-        return (0);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }

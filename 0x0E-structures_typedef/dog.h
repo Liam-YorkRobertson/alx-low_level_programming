da8d8e930d3e77fa72c22bd9f69a2a96949a3d6e
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-20 10:41:06 -0700

    created struct in dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - basic info of dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: name of owner
+ *
+ *Description: basic info of dogs
+ */
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+
+#endif /*dog.h*/

59753632853b1c0db16e2e34ffd65a5f1f4956f4
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-20 12:10:18 -0700

    function that creates a new dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *free_dog - frees teh memory allocated to struct dog
+ *@d: struct dog being freed
+ *Return: none (void)
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d) 
+	{
+		free(d->name);
+		free(d);
+		free(d->owner);
+	}
+}

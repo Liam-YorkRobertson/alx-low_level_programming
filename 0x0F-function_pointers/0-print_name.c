28b137167e271f0a5967ade9e7584314dbc920fb
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-03-22 10:28:33 -0700

    programme that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - prints out a name
+ *@name: name to be printed out
+ *@f: pointer function
+ *Return: none (void)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

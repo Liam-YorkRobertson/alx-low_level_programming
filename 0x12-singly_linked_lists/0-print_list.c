d66c607897aaf3a1cdb6fe75730a9b7828efbc7e
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-03-30 12:12:50 -0700

    programme that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_list - prints elements of list_t list
+ *@h: the singly linked list
+ *Return: the number of elements in list
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nextel;
+
+	nextel = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL) /*str is in main.h, where the struct is*/
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h-> str);
+		}
+		h = h->next;
+		nextel++;
+	}
+	return (nextel);
+}

945041cb7b0d84ab914ba4a6715d12c134f768f0
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-04-04 11:44:26 -0700

    programme that adds a new node at the beginning of a listint_t list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ *add_nodeint - add new node at beginning of singly linked list
+ *@head: pointer to struct
+ *@n: the int to be inserted
+ *Return: pointer to new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode; /*creating in struct*/
+
+	newNode = malloc(sizeof(listint_t)); /*allocating mem*/
+
+	if (newNode == NULL) /*if failure*/
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->n = n; /*newNode = n*/
+	newNode->next = *head; /*next node in struct*/
+
+	*head = newNode; /*values added to struct*/
+	return (*head);
+}

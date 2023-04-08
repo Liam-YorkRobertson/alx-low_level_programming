1ab44c43e28d903a587a02b650887edc22fa16d4
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-04-08 06:19:02 -0700

    programme that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ *print_binary - prints binary from decimal
+ *@n - integer to be changed
+ *Return: none (void)
  */
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1) 
+	if (n > 1)
 	{
-		print_binary(n >> 1);  /* a recursive call */
+		print_binary(n >> 1);  /* a recursive call uses shift bit op*/
 	}
 	_putchar((n & 1) ? '1' : '0');
 }

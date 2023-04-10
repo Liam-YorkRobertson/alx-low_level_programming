a05e8819e37bcca6d4348cbc25f4e28b049a7eec
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-04-10 07:31:24 -0700

    programme that creates a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,14 +33,14 @@ int create_file(const char *filename, char *text_content)
 		{
 			i++;
 		}
+		w = write(fp, text_content, i);
+		if (w == -1)
+		{
+			close(fp);
+			return (-1);
+		}
 	}
 
-	w = write(fp, text_content, i);
-
-	if (w == -1)
-	{
-		return (-1);
-	}
 	close(fp);
 	return (1);
 }

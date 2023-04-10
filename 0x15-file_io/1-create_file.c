9f42bb93372a92dcea9ee09c34f423b76c240b87
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-04-10 07:35:25 -0700

    programme that creates a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	perm = 600; /*permission codei for user has read and write perms*/
+	perm = 0600; /*permission codei for user has read and write perms*/
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, perm);
 
 	if (text_content != NULL)

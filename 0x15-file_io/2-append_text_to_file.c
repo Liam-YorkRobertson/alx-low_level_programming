52c7578df5ebf453eab5940f016a9671d648d3a1
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-04-10 08:10:06 -0700

    progamme that appends text to a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+/**
+ *append_text_to_file - appends text to a file
+ *@filename: file to append to
+ *@text_content: text to append
+ *Return: 1 (success), -1 (failure)
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp;
+	int i, w;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fp = open(filename, O_APPEND);
+
+	if (fp == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		i = 0;
+		while (text_content[i])
+		{
+			i++;
+		}
+		w = write(fp, text_content, i);
+		if (w == -1)
+		{
+			return (-1);
+		}
+	}
+
+	close(fp);
+	return (1);
+}

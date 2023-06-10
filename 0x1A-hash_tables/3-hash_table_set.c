09e26c1f3fbfc385027037c2dd9153a9042c7444
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-06-09 17:21:32 -0700

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,10 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return 0;
-
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-
 	/* Check if the key already exists */
 	temp = ht->array[index];
 	while (temp != NULL)
@@ -27,35 +25,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(temp->value);
 			temp->value = strdup(value);
 			if (temp->value == NULL)
-				return 0;
-			return 1;
+				return (0);
+			return (1);
 		}
 		temp = temp->next;
 	}
-
 	/* Create a new node */
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return 0;
-
+		return (0);
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return 0;
+		return (0);
 	}
-
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
-		return 0;
+		return (0);
 	}
-
-	/* Add the new node at the beginning of the list */
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
-	return 1;
+	return (1);
 }
